418a730e49a1f1bf96936c86d9746d877be4c727
Kazuki Hashimoto <kazukih@tuta.io>
2022-11-23 20:45:21 +0900

    HACK: cpuidle: Don't check if a CPU is isolated
    Those never get applied here.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>

--- drivers/cpuidle/cpuidle.c
+++ drivers/cpuidle/cpuidle.c
@@ -686,7 +686,11 @@ static void wake_up_idle_cpus(void *v)
 static int cpuidle_latency_notify(struct notifier_block *b,
 		unsigned long l, void *v)
 {
-	wake_up_idle_cpus(v);
+	unsigned long cpus = atomic_read(&idled) & *cpumask_bits(to_cpumask(v));
+
+	if (cpus)
+		arch_send_wakeup_ipi_mask(to_cpumask(&cpus));
+
 	return NOTIFY_OK;
 }
 
